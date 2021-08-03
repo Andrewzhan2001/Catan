f1dd254f131841edf776630afe8a2ff9e6db3bcd
t23zhan <t23zhan@uwaterloo.ca>
2021-08-03 16:18:58 -0400

    load method main modify

--- main.cc
+++ main.cc
@@ -30,6 +30,8 @@ int main(int argc, char const *argv[]) {
 
     unique_ptr<Controller> ctr;
     // use find_if to find specific command in the vector
+
+    // here is for player options
     auto it = find_if(command.begin(), command.end(),
       [](const pair<string, string>& element){ return element.first == "-computerAuto";});
     if(it != command.end()) {
@@ -44,6 +46,7 @@ int main(int argc, char const *argv[]) {
       }
     }
 
+
     it = find_if(command.begin(), command.end(),
       [](const pair<string, string>& element){ return element.first == "-seed";});
     if(it != command.end()) {
@@ -55,18 +58,38 @@ int main(int argc, char const *argv[]) {
       [](const pair<string, string>& element){ return element.first == "-load";});
     if(it != command.end()) {
       auto sf = make_unique<Setfromfile>(it.base()->second);
-      ctr->loadStrategy(sf.get());
+      bool check = ctr->loadStrategy(sf.get());
+      if (check == false) {
+        cout << "File does not exist" << endl;
+        return;
+      }
+      
     } else {
       //if do not find command line -load check whether here is -board
       it = find_if(command.begin(), command.end(),
       [](const pair<string, string>& element){ return element.first == "-board";});
       if(it != command.end()) {
         auto fb = make_unique<loadFromBoard>(it.base()->second);
-        ctr->loadStrategy(fb.get());
+        bool check = ctr->loadStrategy(fb.get());
+        if (check == false) {
+          cout << "File does not exist" << endl;
+          return;
+        }
       } else {
         // if both command not found, default randomboard
-        auto rl = make_unique<randomLoad>();
-        ctr->loadStrategy(rl.get());
+        it = find_if(command.begin(), command.end(),
+          [](const pair<string, string>& element){ return element.first == "-random-board";});
+        if(it != command.end()) {
+          auto rl = make_unique<randomLoad>();
+          ctr->loadStrategy(rl.get());
+        } else {
+          auto fb = make_unique<loadFromBoard>("layout.txt");
+          ctr->loadStrategy(fb.get());
+          if (check == false) {
+            cout << "File does not exist" << endl;
+            return;
+          }
+        }
       }
     }
     ctr->print();
