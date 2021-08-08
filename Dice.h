a104bfa85e18c5b928014cca5a29d5d54ef4bb4e
t23zhan <t23zhan@uwaterloo.ca>
2021-08-08 00:42:21 -0400

    comment added

--- Dice.h
+++ Dice.h
@@ -6,7 +6,9 @@
 class Dice {
 public:
 	virtual ~Dice();
+	// set the seed of dice(main use for FairDice)
 	virtual void setseed(size_t seed);
+	// let the dice generate the number(roll the dice)
 	virtual int getNum() = 0;
 };
 #endif
